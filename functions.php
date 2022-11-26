70ddac0f2a46dbe3927a979d07c66acf8363e3cf
Haley B <113158086+Hebishop26@users.noreply.github.com>
2022-11-26 12:00:10 -0600

    Add files via upload

--- functions.php
+++ functions.php
@@ -27,13 +27,96 @@
 	<p><?php echo date(mktime(0,0,0,10,23,2022));?></p>
 	<br>
 	
-	<p>phone number: <?php function formattedNumber($phones){
+
+	<p>
+	<?php
+		$str = "DMACC  ";
 		
+		echo rtrim($str);
+		
+	?>
+	</p>
+	
+	<p>
+	<?php
+		$str = "DMACC  ";
+		echo strtolower($str);
+	?>
+	</p>
+	
+	<p>
+	
+	<?php
+echo strlen($str);
+?>
+	</p>
+	
+	<p>
+	
+	<?php 
+		
+		echo "str is " . is_string($str) . "<br>";
+		
+		?>
+	
+	</p>
+	
+	<p>
+			<!--I did this as well, followed an example found here: https://www.geeksforgeeks.org/php-ctype_upper-function/ -->
+
+		Is it Lowercase or upper case: <?php 
+	
+	if (ctype_upper($str)){
+		
+		echo "Yes/n";
 	}
+		else {
+			
+			echo "No\n";
+				
+		}
+	
+	?></p>
+
+	
+	<br>
+	
+		<p>phone number: <!-- followed a code example on this here on geek for geeks:https://www.geeksforgeeks.org/how-to-format-phone-numbers-in-php/  --> <?php function formatting($phone){
+      
+    // Pass phone number in preg_match function
+    if(preg_match(
+        '/^\+[0-9]([0-9]{3})([0-9]{3})([0-9]{4})$/', 
+    $phone, $value)) {
+      
+        // Store value in format variable
+        $format = $value[1] . '-' . 
+            $value[2] . '-' . $value[3];
+    }
+    else {
+         
+        // If given number is invalid
+        echo "Invalid phone number <br>";
+    }
+      
+    // Print the given format
+    echo("$format" . "<br>");
+	
+	
+}
+	formatting("+1234567890");		
+			
 ?></p>
 	
 	<br>
 	
-	<p><?php echo money_format("price is i%", $number);?></p>
+	<p>
+		<?php 
+	$number = "123456";
+	setlocale(LC_MONETARY, 'en_US');
+echo money_format('%i', $number) . "\n";?>
+	</p>
+	
+	
+	
 </body>
 </html>
